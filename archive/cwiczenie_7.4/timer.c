496525e64e0204f45b1950140c0bbc1f555dff93
filipksiezyc <filipksiezyc123@gmail.com>
2019-05-29 21:11:28 +0200

    poprawki

--- archive/cwiczenie_7.4/timer.c
+++ archive/cwiczenie_7.4/timer.c
@@ -4,9 +4,9 @@
 #define CounterEnable (1<<0)
 #define CounterReset (1<<1)
 #define Register0Interrupt (1<<0)
-#define InterruptFlag (1<<0)
+#define InterruptFlagReset (1<<0)
 #define MatchRegisterReset (1<<1)
-#define PresqualerHex 15
+#define PresqualerValue 15
 
 void InitTimer0(){
 	
@@ -16,19 +16,19 @@ void InitTimer0(){
 void WaitOnTimer0(unsigned int uiTime){
 	T0TCR = (T0TCR | CounterReset);
 	T0TCR = (T0TCR & (~CounterReset));
-	while(T0TC<uiTime*PresqualerHex){		
+	while(T0TC<uiTime*PresqualerValue){		
 	}
 }
 
 void InitTimer0Match0(unsigned int iDelayTime){ 
-	T0MR0 = (PresqualerHex*iDelayTime);
+	T0MR0 = (PresqualerValue*iDelayTime);
 	T0MCR = (T0MCR | Register0Interrupt | MatchRegisterReset);
 	T0TCR = (T0TCR | CounterReset);
 	T0TCR = T0TCR & (~CounterReset);
 	T0TCR = T0TCR | CounterEnable;
 }
 void WaitOnTimer0Match0(){
-	while((T0IR & InterruptFlag) == 0) {
+	while((T0IR & InterruptFlagReset) == 0) {
 	}
-	T0IR = InterruptFlag;
+	T0IR = InterruptFlagReset;
 }
